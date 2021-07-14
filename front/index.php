d43f692e35f80d12462a1815f4abf3b6862046a8
jade <50565661+jadeeeeee@users.noreply.github.com>
2021-07-14 14:04:44 -0300

    primeiro commit

---
+++ front/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-be">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link rel='stylesheet' type='text/css' media='screen' href='../css/css.css'>
+    <link rel="shortcut icon" href="../imgs/livro3.png"/>
+    <title> INÍCIO </title>
+</head>
+<body>
+<div class='cabeçalho'>
+        <h1 class="titulo"> IF LEITURA </h1> <break>
+        <ul>
+        <!-- aqui o user só pode entrar logado-->
+        <li> <a href="home.php"> LIVROS </a> </li>
+        <li > <a href="cadastroF.php"> CADASTRO </a> </li>
+        <li> <a href="loginF.php"> LOGIN </a> </li>
+        </ul>
+    </div>
+
+    <div id='index'>
+        <h2> Seja bem vindo ao IF Leitura! </h2>
+        <p> Aqui você encontrará títulos da biblioteca do Instituto Federal Baiano Campus Guanambi e poderá expressar sua opnião sobre eles. </p>
+    </div>
+</body>
+</html>

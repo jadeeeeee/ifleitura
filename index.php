8ebf37cf4a46d34104a00d77cbe9adb8a4ac3f4d
jade <50565661+jadeeeeee@users.noreply.github.com>
2021-07-15 23:20:09 -0300

    att diretorios

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-be">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link rel='stylesheet' type='text/css' media='screen' href='css/css.css'>
+    <link rel="shortcut icon" href="imgs/livro3.png"/>
+    <title> INÍCIO </title>
+</head>
+<body>
+
+    <div class="conteudo-index">
+        <div class="nome">
+            <h1>IF LEITURA</h1> <h2>O CLUBE DE LEITURA ONLINE DO IF BAIANO - CAMPUS GUANAMBI</h2>
+        </div>
+        <div class='textin'>
+            <h2> Sobre o IF Leitura </h2>
+            <p> Olá, seja bem vindo(a) ao IF Leitura! Essa plataforma tem o objetivo de servir como um clube de leitura para os estudantes leitores do IF Baiano Campus Guanambi. <br> bla bla bla bla bla bla bla</p>
+        </div>
+    </div>
+    <div class='index-menu'>
+        <ul>
+        <!-- aqui o user só pode entrar logado-->
+        <li> <a href="php/home.php"> LIVROS </a> </li>
+        <li > <a href="php/cadastro.php"> CADASTRO </a> </li>
+        <li> <a href="php/login.php"> LOGIN </a> </li>
+        <li> <a href=""> PERFIL </a> </li>
+        </ul>
+    </div>
+</body>
+</html>

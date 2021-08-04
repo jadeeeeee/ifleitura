ceb048c8bee7da289396417ecde707ef4b9b1b6e
jade <50565661+jadeeeeee@users.noreply.github.com>
2021-08-04 20:38:57 -0300

    finalizando front

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <link rel='stylesheet' type='text/css' media='screen' href='css/css.css'>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans&display=swap" rel="stylesheet">
     <link rel="shortcut icon" href="imgs/livro3.png"/>
     <title> INÍCIO </title>
 </head>
@@ -12,20 +15,23 @@
 
     <div class="conteudo-index">
         <div class="nome">
-            <h1>IF LEITURA</h1> <h2>O CLUBE DE LEITURA ONLINE DO IF BAIANO - CAMPUS GUANAMBI</h2>
+            <!-- <img src="imgs/ifleitura.png" alt="Logo" id="logo"> -->
+            <h1 id = "red">i</h1> <h1 id = "green">f</h1> <h1 class = "titulo">leitura</h1> <br>
+            <h2 class= "subtitulo"> o clube de leitura online do if baiano </h2>
         </div>
-        <div class='textin'>
-            <h2> Sobre o IF Leitura </h2>
-            <p> Olá, seja bem vindo(a) ao IF Leitura! Essa plataforma tem o objetivo de servir como um clube de leitura para os estudantes leitores do IF Baiano Campus Guanambi. <br> bla bla bla bla bla bla bla</p>
+        
+        <div class='index-menu'>
+            <ul>
+            <li> <a href="php/home.php"> livros </a> </li>
+            <li > <a href="php/cadastro.php"> cadastro </a> </li>
+            <li> <a href="php/login.php"> login </a> </li>
+            </ul>
         </div>
-    </div>
-    <div class='index-menu'>
-        <ul>
-        <li> <a href="php/home.php"> LIVROS </a> </li>
-        <li > <a href="php/cadastro.php"> CADASTRO </a> </li>
-        <li> <a href="php/login.php"> LOGIN </a> </li>
-        <li> <a href=""> PERFIL </a> </li>
-        </ul>
+
+        <!-- <div class='textin'>
+            <h2> sobre o if leitura </h2>
+            <p> olá, seja bem vindo(a) ao IF Leitura! Essa plataforma tem o objetivo de servir como um clube de leitura para os estudantes leitores do IF Baiano Campus Guanambi. <br> bla bla bla bla bla bla bla</p>
+        </div> -->
     </div>
 </body>
 </html>
